e296e78ad92ba55151693d35a9681c38972220fa
rezanurrochmat@gmail.com <rezanurrochmat@gmail.com>
2019-12-07 22:20:21 +0700

    [Rejak] Adding new method arithmatic

--- function.ex
+++ function.ex
@@ -2,6 +2,27 @@ defmodule CustomMathFunction do
   def addition(a, b) do
     a + b
   end
+
+  def substraction(a, b) do
+    a - b
+  end
+
+  def multiplication(a, b) do
+    a * b
+  end
+
+  def modulo(a, b) do
+    Integer.mod(a, b)
+  end
+
+  def division(a, b) do
+    a / b
+  end
 end
 
+# Calling the function
 IO.puts(CustomMathFunction.addition(5, 6))
+IO.puts(CustomMathFunction.substraction(-4, -9))
+IO.puts(CustomMathFunction.multiplication(7, 9))
+IO.puts(CustomMathFunction.modulo(6, 2))
+IO.puts(CustomMathFunction.division(4, 2))
